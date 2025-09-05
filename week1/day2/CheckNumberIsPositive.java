8d1dec963c2674fafe0bc50e378936dc6ac95f7e
monica06218 <vinitharaghavan123@gmail.com>
2025-09-05 09:48:45 +0200

    Add files via upload
    week1

---
+++ week1/day2/CheckNumberIsPositive.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int version=10;
+		if(version>=0) {
+			System.out.println("Number is positive");
+		}
+		else
+		{
+			System.out.println("Number is negative");
+		}
+ 
+	}
+
+}
