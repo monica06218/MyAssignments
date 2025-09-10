2a034bfbfa7ecd62ae20b4a61e978d9cca5322b6
monica06218 <vinitharaghavan123@gmail.com>
2025-09-10 23:03:37 +0200

    Add files via upload
    week2

---
+++ week2/day1/day1/EdgeBrowser.java
@@ -0,0 +1,12 @@
+package week2.day1;
+public class EdgeBrowser {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Browser b=new Browser();
+          System.out.println(b.launchBrowser("chrome"));
+          b.loadUrl();
+	}
+
+}
